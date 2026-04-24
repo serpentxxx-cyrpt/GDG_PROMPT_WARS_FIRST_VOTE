d10744fba39165f3ff86376255848550976ed22a
Tridibesh Sen <tridibeshsen2002@gmail.com>
2026-04-24 23:55:08 +0530

    fix: React import in Settings.jsx + CI/CD workflow + Firebase hosting config

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useGame } from "../context/GameContext";
 import { loginWithGoogle, logout } from "../services/firebase";
